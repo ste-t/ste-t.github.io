65d36f435f11a28867865dd2e5e8b9076601c5e7
Stefan <ilmastrostefanuzzo@gmail.com>
2023-07-23 15:00:36 +0200

    Copy from old repo

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const typewriter = new Typewriter("#hello-friend-text", {
+    strings: [
+        "Hello friend",
+        "Ciao amico",
+        "Bok prijatelju",
+        "Привет друг",
+        "Hallo Freund",
+        "Hej ven",
+        "Привіт друже",
+        "Salut l'ami",
+        "Γεια σου φίλε",
+        "Hola amigo",
+        !Math.random() ? "Сәләм дуҫ" : "Salut prietene",
+    ],
+    cursor: "_",
+    cursorClassName: "typewriter-cursor", // Give it a nice name and remove the default CSS
+    autoStart: true,
+    loop: true,
+    delay: 80,
+    pauseFor: 750,
+});
